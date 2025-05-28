61e85a4c34af93427fffee7f379bb19769ce410c
강인성 <shawn9272@naver.com>
2025-05-28 23:07:16 +0900

    springboot 9주차 과제

--- src/main/java/mvc/example/demo/entity/User.java
+++ src/main/java/mvc/example/demo/entity/User.java
@@ -2,7 +2,6 @@ package mvc.example.demo.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
